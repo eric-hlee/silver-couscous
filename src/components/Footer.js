375baaba4711abba3ce708b16013fbf549012453
Eric Lee <ericlee1995@hotmail.com>
2020-05-22 20:03:34 +0800

    Add Instagram link to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
 
 export default function Footer() {
   return (
@@ -11,27 +14,46 @@ export default function Footer() {
         </span>
         .
       </p>
-      <Button
-        className="contact-item icon-mail-alt"
-        href="mailto:hsuan9528@gmail.com"
-        variant="outline-dark"
-      >
-        Email
-      </Button>
-      <Button
-        className="contact-item icon-linkedin"
-        href="http://www.linkedin.com/in/erichsuanlee"
-        variant="outline-dark"
-      >
-        LinkedIn
-      </Button>
-      <Button
-        className="contact-item icon-github"
-        href="http://github.com/ericlee1995"
-        variant="outline-dark"
-      >
-        GitHub
-      </Button>
+      <Container className="contact-item-container">
+        <Row xs={2} md={4} lg={6}>
+          <Col>
+            <Button
+              className="contact-item icon-mail-alt"
+              href="mailto:hsuan9528@gmail.com"
+              variant="outline-dark"
+            >
+              Email
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              className="contact-item icon-linkedin"
+              href="http://www.linkedin.com/in/erichsuanlee"
+              variant="outline-dark"
+            >
+              LinkedIn
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              className="contact-item icon-github"
+              href="http://github.com/ericlee1995"
+              variant="outline-dark"
+            >
+              GitHub
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              className="contact-item icon-instagram"
+              href="http://www.instagram.com/eric.hlee/"
+              variant="outline-dark"
+            >
+              Instagram
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   )
 }
