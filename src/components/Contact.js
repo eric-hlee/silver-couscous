c06248caa06274178f2c5c33230d17769b8537f3
Eric Lee <ericlee1995@hotmail.com>
2020-06-02 18:53:12 +0800

    Fix form submission error

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,68 +4,117 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 
 export default function Contact() {
-	const [validated, setValidated] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [validated, setValidated] = useState(false)
 
-	const handleSubmit = (event) => {
-		const form = event.currentTarget
-		if (form.checkValidity() === false) {
-			event.preventDefault()
-			event.stopPropagation()
-		}
+  const encode = (data) => {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+      )
+      .join('&')
+  }
 
-		setValidated(true)
-	}
+  const handleSubmit = (e) => {
+    const form = e.currentTarget
+    if (form.checkValidity() === false) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
 
-	return (
-		<section className="contact-form">
-			<h1 className="contact">Contact me</h1>
-			<Form
-				name="contact"
-				method="post"
-				className="contact"
-				noValidate
-				validated={validated}
-				onSubmit={handleSubmit}
-			>
-				<Form.Group controlId="formName&Email">
-					<Form.Row>
-						<Col>
-							<Form.Label>Name</Form.Label>
-							<Form.Control
-								type="text"
-								name="name"
-								placeholder="Name"
-								required
-							/>
-							<Form.Control.Feedback type="invalid">
-								Please enter your name.
-							</Form.Control.Feedback>
-						</Col>
-						<Col>
-							<Form.Label>Email address</Form.Label>
-							<Form.Control
-								type="email"
-								name="email"
-								placeholder="Enter email"
-								required
-							/>
-							<Form.Control.Feedback type="invalid">
-								Please provide a valid email.
-							</Form.Control.Feedback>
-						</Col>
-					</Form.Row>
-				</Form.Group>
-				<Form.Group controlId="formMessage">
-					<Form.Label>Message</Form.Label>
-					<Form.Control as="textarea" name="message" rows="3" required />
-					<Form.Control.Feedback type="invalid">
-						Please enter a message.
-					</Form.Control.Feedback>
-				</Form.Group>
-				<Button className="submit-button" variant="dark" type="submit">
-					Submit
-				</Button>
-			</Form>
-		</section>
-	)
+    setValidated(true)
+
+    const data = { name, email, message }
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...data }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error))
+
+    e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value)
+        break
+      case 'email':
+        setEmail(e.target.value)
+        break
+      case 'message':
+        setMessage(e.target.value)
+        break
+      default:
+        console.error('An error occurred while processing form data.')
+    }
+  }
+
+  return (
+    <section className="contact-form">
+      <h1 className="contact">Contact me</h1>
+      <Form
+        name="contact"
+        method="post"
+        className="contact"
+        noValidate
+        validated={validated}
+        onSubmit={handleSubmit}
+      >
+        <Form.Group controlId="formName&Email">
+          <Form.Row>
+            <Col>
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                value={name}
+                placeholder="Name"
+                onChange={handleChange}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please enter your name.
+              </Form.Control.Feedback>
+            </Col>
+            <Col>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                value={email}
+                placeholder="Enter email"
+                onChange={handleChange}
+                required
+              />
+              <Form.Control.Feedback type="invalid">
+                Please provide a valid email.
+              </Form.Control.Feedback>
+            </Col>
+          </Form.Row>
+        </Form.Group>
+        <Form.Group controlId="formMessage">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="message"
+            value={message}
+            rows="3"
+            onChange={handleChange}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a message.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button className="submit-button" variant="dark" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </section>
+  )
 }
