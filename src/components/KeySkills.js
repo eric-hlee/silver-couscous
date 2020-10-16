89128aa4aa6dacadbe90cd606328e8c3627a1537
Eric Lee <ericlee1995@hotmail.com>
2020-10-16 00:28:53 -0700

    Refactor KeySkills component

--- src/components/KeySkills.js
+++ src/components/KeySkills.js
@@ -1,49 +1,46 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-export default function KeySkills () {
+export default function KeySkills() {
+  const getSkillItem = (skill, index) => {
+    return <ListGroup.Item key={index}>{skill}</ListGroup.Item>;
+  };
+
   return (
     <Card className="key-skills">
       <Card.Body>
         <Card.Text as="div">
           <Card.Title>Programming languages:</Card.Title>
           <ListGroup horizontal="sm">
-            <ListGroup.Item>Python</ListGroup.Item>
-            <ListGroup.Item>Java</ListGroup.Item>
-            <ListGroup.Item>Bash</ListGroup.Item>
+            {['Python', 'Java', 'Bash'].map(getSkillItem)}
           </ListGroup>
           <br />
           <Card.Title>Web/scripting languages:</Card.Title>
           <ListGroup horizontal="sm">
-            <ListGroup.Item>HTML</ListGroup.Item>
-            <ListGroup.Item>CSS</ListGroup.Item>
-            <ListGroup.Item>Sass</ListGroup.Item>
-            <ListGroup.Item>JavaScript</ListGroup.Item>
-            <ListGroup.Item>SQL</ListGroup.Item>
+            {['HTML', 'CSS', 'Sass', 'JavaScript', 'SQL'].map(getSkillItem)}
           </ListGroup>
           <br />
           <Card.Title>Library/Frameworks:</Card.Title>
           <ListGroup horizontal="sm">
-            <ListGroup.Item>ReactJS</ListGroup.Item>
-            <ListGroup.Item>Redux</ListGroup.Item>
-            <ListGroup.Item>React-Router</ListGroup.Item>
-            <ListGroup.Item>React Intl</ListGroup.Item>
-            <ListGroup.Item>ExpressJS</ListGroup.Item>
-            <ListGroup.Item>DraftJS</ListGroup.Item>
-            <ListGroup.Item>Jest</ListGroup.Item>
-            <ListGroup.Item>Enzyme</ListGroup.Item>
+            {[
+              'ReactJS',
+              'Redux',
+              'React-Router',
+              'React Intl',
+              'ExpressJS',
+              'DraftJS',
+              'Jest',
+              'Enzyme',
+            ].map(getSkillItem)}
           </ListGroup>
           <br />
           <Card.Title>Other technologies:</Card.Title>
           <ListGroup horizontal="sm">
-            <ListGroup.Item>Docker</ListGroup.Item>
-            <ListGroup.Item>Git</ListGroup.Item>
-            <ListGroup.Item>GitHub</ListGroup.Item>
-            <ListGroup.Item>Webpack</ListGroup.Item>
+            {['Docker', 'Git', 'GitHub', 'Webpack'].map(getSkillItem)}
           </ListGroup>
         </Card.Text>
       </Card.Body>
     </Card>
-  )
+  );
 }
