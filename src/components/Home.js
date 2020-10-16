1bc1b2442c31136d09edd54ca0417fc01242f667
Eric Lee <ericlee1995@hotmail.com>
2020-10-16 14:05:55 -0700

    Refactor Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import { FormattedMessage } from 'react-intl'
-import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-import Image from 'react-bootstrap/Image'
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Row from 'react-bootstrap/Row'
-import About from './About'
-import Contact from './Contact'
-import Footer from './Footer'
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Row from 'react-bootstrap/Row';
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
 
 export default function Home() {
+  const getLifeAsDevItem = (item) => {
+    return (
+      <li>
+        <FormattedMessage id={item.id} defaultMessage={item.defaultMessage} />
+      </li>
+    );
+  };
+
   return (
     <React.Fragment>
       <main>
@@ -60,48 +68,40 @@ export default function Home() {
                     </strong>
                   </p>
                   <ul>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_1"
-                        defaultMessage="Started learning HTML at 10"
-                      />
-                    </li>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_2"
-                        defaultMessage="Received an award for creating a website at 12"
-                      />
-                    </li>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_3"
-                        defaultMessage="Studied computer science in university at 18"
-                      />
-                    </li>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_4"
-                        defaultMessage="Completed an internship at 22"
-                      />
-                    </li>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_5"
-                        defaultMessage="Kicked off career as a technical support engineer at 23"
-                      />
-                    </li>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_6"
-                        defaultMessage="Joined a startup as a junior software engineer at 24"
-                      />
-                    </li>
-                    <li>
-                      <FormattedMessage
-                        id="home.life_as_dev_7"
-                        defaultMessage="Got laid off due to Covid-19 and looking for new opportunities!"
-                      />
-                    </li>
+                    {[
+                      {
+                        id: 'home.life_as_dev_1',
+                        defaultMessage: 'Started learning HTML at 10',
+                      },
+                      {
+                        id: 'home.life_as_dev_2',
+                        defaultMessage:
+                          'Received an award for creating a website at 12',
+                      },
+                      {
+                        id: 'home.life_as_dev_3',
+                        defaultMessage:
+                          'Studied computer science in university at 18',
+                      },
+                      {
+                        id: 'home.life_as_dev_4',
+                        defaultMessage: 'Completed an internship at 22',
+                      },
+                      {
+                        id: 'home.life_as_dev_5',
+                        defaultMessage:
+                          'Kicked off career as a technical support engineer at 23',
+                      },
+                      {
+                        id: 'home.life_as_dev_6',
+                        defaultMessage:
+                          'Joined a startup as a junior software engineer at 24',
+                      },
+                      {
+                        id: 'home.life_as_dev_7',
+                        defaultMessage: 'Looking for new opportunities!',
+                      },
+                    ].map(getLifeAsDevItem)}
                   </ul>
                 </div>
               </Col>
@@ -113,5 +113,5 @@ export default function Home() {
       </main>
       <Footer />
     </React.Fragment>
-  )
+  );
 }
